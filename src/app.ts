28e8868b3c5742ff3361385b1a5c4ad458922ff7
radin-111 <radinsheikh87@gmail.com>
2026-03-22 21:37:25 +0600

    making some changes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { indexRoutes } from './app/routes';
 
 
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.use("/api/v1", indexRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
