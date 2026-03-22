e3c493569f4122eea8be47fb9bc55e7f0a4e2143
radin-111 <radinsheikh87@gmail.com>
2026-03-22 21:46:12 +0600

    adding required packages

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,3 +1,17 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
+
+
+interface envConfig{
+  DATABASE_URL: string;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
+  PORT: string;
+  APP_URL: string;
+  APP_USER: string;
+  APP_PASS: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  NODE_ENV: string;
+}
